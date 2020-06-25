af59b0bf4d8acd46efe2a8afea4a419159b7f2dd
JoaoGranja <joaogranja10@hotmail.com>
2020-06-25 18:14:10 +0000

    Copping the top and bottom of the images

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import os, csv, cv2
 import numpy as np
 from scipy import ndimage
+import tensorflow as tf
 
 # Loading data
 lines = []
@@ -10,16 +11,18 @@ with open("../data/data_22_06/driving_log.csv") as csvfile:
     for line in reader:
         lines.append(line)
         
+correction = [0, 0.2, -0.2]
 images = []
 labels = []
 for line in lines:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = '../data/data_22_06/IMG/' + filename
-    image = ndimage.imread(current_path)  
-    images.append(image)
-    
-    labels.append(float(line[3]))
+    for i in range(3):
+        source_path = line[i]
+        filename = source_path.split('/')[-1]
+        current_path = '../data/data_22_06/IMG/' + filename
+        image = ndimage.imread(current_path)  
+        images.append(image)
+
+        labels.append(float(line[3]) + correction[i])
  
 print("Images length is", len(images))
 print("Labels length is", len(labels), "minimum value is {0} and maximum valus is {1}".format(min(labels), max(labels)))
@@ -33,60 +36,42 @@ for image, label in zip(images, labels):
     augmented_images.append(cv2.flip(image,1))
     augmented_labels.append(label*-1.0)
 
-X_train = np.array(images)
-Y_train = np.array(labels)
+X_train = np.array(augmented_images)
+Y_train = np.array(augmented_labels)
 
 ###--------------------------------- Neural Network Model ------------------------------------------------------ ###
 from keras.applications.inception_v3 import InceptionV3, preprocess_input
 from keras.preprocessing import image
-from keras.models import Model
-from keras.layers import Dense, GlobalAveragePooling2D, Lambda, Input
+from keras.models import Model, Sequential
+from keras.layers import Dense, GlobalAveragePooling2D, Lambda, Input, Flatten, Conv2D, MaxPooling2D, Activation, Cropping2D
 from keras.preprocessing.image import ImageDataGenerator
 
-debug = False
+debug = True
 batch_size = 32
-epochs = 2
-
-# PreProcessing input data
-train_datagen = ImageDataGenerator(rescale=1./255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    preprocessing_function=preprocess_input) 
-
-train_datagen.fit(X_train)
-
-train_generator = train_datagen.flow(
-    X_train, 
-    Y_train, 
-    batch_size=batch_size)
+epochs = 7
 
 '''
-validation_generator = train_datagen.flow(
-    x_train, 
-    y_train, 
-    batch_size=batch_size,
-    subset='validation') # set as validation data
-'''
-
-# Create the base pre-trained model
-inception = InceptionV3(weights='imagenet', include_top=False,
-                        input_shape=(160,320,3))
-
-# Freeze all convolutional InceptionV3 layers
-for layer in inception.layers:
-    layer.trainable = False
-
 # Input Layer
 inputs = Input(shape=(160,320,3))
 
+# Create the base pre-trained model
+inception_model = InceptionV3(weights='imagenet', include_top=False,
+                        input_shape=(160,320,3))
+
+# Freeze all convolutional InceptionV3 layers
+for layer in inception_model.layers:
+    layer.trainable = False
+
 # Pre-process the input with Kera's Lambda layer
-preprocessed_input = Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3))
+preprocessed_inputs = Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3))
+
+
+tf.reset_default_graph()
 
 # Add the top layers
-x = preprocessed_input(inputs)
-x = inception(x)
-## test x = inception.output
+#x = preprocessed_inputs(inputs)
+x = inception_model(inputs)
+#x = inception_model.output
 end_pooling = GlobalAveragePooling2D()(x)
 x = Dense(512, activation='relu')(end_pooling)
 prediction = Dense(1, activation='linear')(x)
@@ -94,9 +79,35 @@ prediction = Dense(1, activation='linear')(x)
 # Build the model to train
 ## test model = Model(inputs=inception.input, outputs=prediction)
 model = Model(inputs=inputs, outputs=prediction)
+'''
+ 
+# 1 - Model
+model = Sequential()
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((50, 20), (0, 0))))
+model.add(Conv2D(filters=32, kernel_size=(5,5), strides=(2,2), padding='valid'))
+model.add(Activation('relu'))
+# Max Pooling
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+
+model.add(Conv2D(filters=96, kernel_size=(5,5), strides=(2,2), padding='valid'))
+model.add(Activation('relu'))
+# Max Pooling
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+
+model.add(Conv2D(filters=252, kernel_size=(5,5), strides=(2,2), padding='valid'))
+model.add(Activation('relu'))
+# Max Pooling
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
+model.add(Dense(1))
 
 # compile the model 
-model.compile(optimizer='rmsprop', loss='mean_squared_error')
+#model.compile(optimizer='rmsprop', loss='mean_squared_error')
+model.compile(optimizer='adam', loss='mse')
 
 if debug:
     print("Model summary:")
@@ -107,7 +118,6 @@ if debug:
 model.fit(
     X_train,
     Y_train,
-    batch_size = batch_size ,
     validation_split = 0.2,
     epochs = epochs, 
     shuffle = True)
