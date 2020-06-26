9fbbd0e98c975e494b0bc02726487c5a9990a297
JoaoGranja <joaogranja10@hotmail.com>
2020-06-26 18:32:40 +0000

    Add more data to training the model. Try a different neural model parameters

--- main.py
+++ main.py
@@ -4,29 +4,30 @@ import numpy as np
 from scipy import ndimage
 import tensorflow as tf
 
-# Loading data
-lines = []
-with open("../data/data_22_06/driving_log.csv") as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        lines.append(line)
-        
+# Loading data from several sources
+source_paths = ["../data/data_22_06/", "../data/data_26_06/","data/"]
 correction = [0, 0.2, -0.2]
 images = []
 labels = []
-for line in lines:
-    for i in range(3):
-        source_path = line[i]
-        filename = source_path.split('/')[-1]
-        current_path = '../data/data_22_06/IMG/' + filename
-        image = ndimage.imread(current_path)  
-        images.append(image)
+for path in source_paths:
+    print(path)
+    lines = []
+    with open(path+"driving_log.csv") as csvfile:
+        reader = csv.reader(csvfile)
+        if path == "data/":
+            next(reader)
+        for line in reader:
+            lines.append(line)
 
-        labels.append(float(line[3]) + correction[i])
- 
-print("Images length is", len(images))
-print("Labels length is", len(labels), "minimum value is {0} and maximum valus is {1}".format(min(labels), max(labels)))
+    for line in lines:
+        for i in range(3):
+            source_path = line[i]
+            filename = source_path.split('/')[-1]
+            current_path = path +'IMG/' + filename
+            image = ndimage.imread(current_path)  
+            images.append(image)
 
+            labels.append(float(line[3]) + correction[i])
 
 ## Data Augmentation
 augmented_images, augmented_labels = [], []
@@ -39,16 +40,21 @@ for image, label in zip(images, labels):
 X_train = np.array(augmented_images)
 Y_train = np.array(augmented_labels)
 
+print("Images length is", len(augmented_images))
+print("Labels length is", len(augmented_labels), "minimum value is {0} and maximum valus is {1}".format(min(augmented_labels), max(augmented_labels)))
+
+
 ###--------------------------------- Neural Network Model ------------------------------------------------------ ###
 from keras.applications.inception_v3 import InceptionV3, preprocess_input
 from keras.preprocessing import image
 from keras.models import Model, Sequential
 from keras.layers import Dense, GlobalAveragePooling2D, Lambda, Input, Flatten, Conv2D, MaxPooling2D, Activation, Cropping2D
+from keras.layers import Dropout
 from keras.preprocessing.image import ImageDataGenerator
 
 debug = True
 batch_size = 32
-epochs = 7
+epochs = 2
 
 '''
 # Input Layer
@@ -81,27 +87,30 @@ prediction = Dense(1, activation='linear')(x)
 model = Model(inputs=inputs, outputs=prediction)
 '''
  
-# 1 - Model
+# Build a Sequential Model
 model = Sequential()
-model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
-model.add(Cropping2D(cropping=((50, 20), (0, 0))))
-model.add(Conv2D(filters=32, kernel_size=(5,5), strides=(2,2), padding='valid'))
+model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+model.add(Conv2D(filters=32, kernel_size=(5,5), strides=(1,1), padding='valid'))
 model.add(Activation('relu'))
 # Max Pooling
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+model.add(Dropout(0.5))
 
-model.add(Conv2D(filters=96, kernel_size=(5,5), strides=(2,2), padding='valid'))
+model.add(Conv2D(filters=96, kernel_size=(5,5), strides=(1,1), padding='valid'))
 model.add(Activation('relu'))
 # Max Pooling
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+model.add(Dropout(0.5))
 
-model.add(Conv2D(filters=252, kernel_size=(5,5), strides=(2,2), padding='valid'))
+model.add(Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), padding='valid'))
 model.add(Activation('relu'))
 # Max Pooling
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+model.add(Dropout(0.5))
 
 model.add(Flatten())
-model.add(Dense(120))
+model.add(Dense(512))
 model.add(Dense(84))
 model.add(Dense(1))
 
@@ -125,3 +134,16 @@ model.fit(
 # Save the model
 model.save('model.h5')
 
+'''
+### print the keys contained in the history object
+print(model.history.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(model.history['loss'])
+plt.plot(model.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
+'''
